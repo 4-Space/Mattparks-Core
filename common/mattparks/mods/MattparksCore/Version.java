59c7e6b5cbb622eae48d253d650d1b37e7ab3d6e
Matthew Albrecht <mattparks5855@gmail.com>
2014-06-16 21:19:19 -0500

    Upload new files

---
+++ common/mattparks/mods/MattparksCore/Version.java
@@ -0,0 +1,13 @@
+package mattparks.mods.MattparksCore;
+
+public class Version
+{
+	public static final int LOCALMAJVERSION = 2;
+	public static final int LOCALMINVERSION = 0;
+	public static final int LOCALBUILDVERSION = 7;
+	public static final int LOCALGITVERSION = 18; //UPDATES EVERY BUILD
+	public static int remoteMajVer;
+	public static int remoteMinVer;
+	public static int remoteBuildVer;
+	public static int remoteGITVer;
+}
